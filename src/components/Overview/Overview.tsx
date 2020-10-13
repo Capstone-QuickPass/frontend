24be13c4912ec8ec7ac54d9b691ddb10a7d5729a
Daniel Shwan <dannyshwan@gmail.com>
2020-10-13 00:20:48 -0400

    reorganized

---
+++ src/components/Overview/Overview.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Overview = () => {
+   return(
+      <div></div>
+   );
+}
+
+export default Overview;
---
+++ src/views/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dashboard = () => {
+   return(
+      <div>yeet</div>
+   );
+}
+
+export { Dashboard };
