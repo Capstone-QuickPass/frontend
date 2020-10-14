c715a67edb9c3400f0c0e4ad12b0dfe4e78ba1c2
Daniel Shwan <dannyshwan@gmail.com>
2020-10-14 11:35:18 -0400

    renamed app to App

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.scss';
-import App from './app';
+import App from './App';
 
 ReactDOM.render(
   <BrowserRouter>
