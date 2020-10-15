91b6f1c1ecbd86012e6061154e4c605e90ef5f62
Daniel Shwan <dannyshwan@gmail.com>
2020-10-15 18:19:10 -0400

    added jest with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './styles/app.scss';
+import './styles/App.scss';
 import styled from 'styled-components';
 import { Router, Sidebar, Header } from './components';
 
