1be0d5a822d748b4489613a7ddb639beff6d82f3
Keith Tang <keithtang28@gmail.com>
2020-10-14 20:16:30 -0400

    Tile with counter that increments

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,16 +1,62 @@
-import React from 'react';
+import React, { useState , useEffect} from 'react';
 import styled from 'styled-components';
-
-const Dashboard = () => {
-   return(
-      <Display>SAMPLE</Display>
-   );
-}
 
 const Display = styled.div`
    display: flex;
    justify-content: center;
    align-items: center;
 `
+
+const TileContainer = styled.div`
+   height: 100px;
+   width: 200px;
+   background-color: ghostwhite;
+   box-shadow: 2px 2px gainsboro;
+   padding: 20px;
+   border-radius: 5px;
+   text-align: right;
+`
+
+const CountTitle = styled.p`
+   font-size: 25px;
+   color: dimgray;
+   font-family: 'Courier New', Courier, monospace;
+   margin: 0;
+`
+
+const NumberDisplay = styled.p`
+   font-size: 35px;
+   margin: 20px;
+`
+
+const TileCounter = () => {
+
+   const [count, setCount] = useState(69);
+
+   useEffect(() => {
+      const interval = setInterval(() => {
+      setCount(count => count + 1);
+    }, 1500);
+      return () => {
+      clearInterval(interval);
+    };
+   }, []);
+      
+   return(
+      <TileContainer>
+         <CountTitle>Users</CountTitle>
+         <NumberDisplay>{count}</NumberDisplay>
+      </TileContainer>
+   )
+}
+
+const Dashboard = () => {
+   return(
+      <Display>
+         {TileCounter()}
+      </Display>
+   );
+}
+
 
 export { Dashboard };
