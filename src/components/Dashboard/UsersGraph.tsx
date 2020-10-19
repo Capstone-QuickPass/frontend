8b78c46941e0bf5358040d6c551abaa91a8b0e01
Daniel Shwan <dannyshwan@gmail.com>
2020-10-18 21:42:26 -0400

    renamed to at risk

--- src/components/Dashboard/UsersGraph.tsx
+++ src/components/Dashboard/UsersGraph.tsx
@@ -23,46 +23,46 @@ const GraphTitle = styled.p`
 
 const data = [
    {
-     name: 'Sept. 25, 2020', Users: 105, "COVID-19 Cases": 2
+     name: 'Sept. 25, 2020', Users: 105, "At Risk Cases": 2
    },
    {
-     name: 'Sept. 26, 2020', Users: 35, "COVID-19 Cases": 0
+     name: 'Sept. 26, 2020', Users: 35, "At Risk Cases": 0
    },
    {
-     name: 'Sept. 27, 2020', Users: 57, "COVID-19 Cases": 1
+     name: 'Sept. 27, 2020', Users: 57, "At Risk Cases": 1
    },
    {
-     name: 'Sept. 28, 2020', Users: 69, "COVID-19 Cases": 10
+     name: 'Sept. 28, 2020', Users: 69, "At Risk Cases": 10
    },
    {
-     name: 'Sept. 29, 2020', Users: 12, "COVID-19 Cases": 0
+     name: 'Sept. 29, 2020', Users: 12, "At Risk Cases": 0
    },
    {
-     name: 'Sept. 30, 2020', Users: 48, "COVID-19 Cases": 0
+     name: 'Sept. 30, 2020', Users: 48, "At Risk Cases": 0
    },
    {
-     name: 'Oct. 1, 2020', Users: 86, "COVID-19 Cases": 0
+     name: 'Oct. 1, 2020', Users: 86, "At Risk Cases": 0
    },
    {
-     name: 'Oct. 2, 2020', Users: 68, "COVID-19 Cases": 0
+     name: 'Oct. 2, 2020', Users: 68, "At Risk Cases": 0
    },
    {
-     name: 'Oct. 3, 2020', Users: 145, "COVID-19 Cases": 10
+     name: 'Oct. 3, 2020', Users: 145, "At Risk Cases": 10
    },
    {
-     name: 'Oct. 4, 2020', Users: 34, "COVID-19 Cases": 2
+     name: 'Oct. 4, 2020', Users: 34, "At Risk Cases": 2
    },
    {
-     name: 'Oct. 5, 2020', Users: 105, "COVID-19 Cases": 3
+     name: 'Oct. 5, 2020', Users: 105, "At Risk Cases": 3
    },
    {
-     name: 'Oct. 6, 2020', Users: 280, "COVID-19 Cases": 50
+     name: 'Oct. 6, 2020', Users: 280, "At Risk Cases": 50
    },
    {
-     name: 'Oct. 7, 2020', Users: 56, "COVID-19 Cases": 0
+     name: 'Oct. 7, 2020', Users: 56, "At Risk Cases": 0
    },
    {
-     name: 'Oct. 8, 2020', Users: 100, "COVID-19 Cases": 1
+     name: 'Oct. 8, 2020', Users: 100, "At Risk Cases": 1
    },
  ];
 
@@ -84,7 +84,7 @@ const UsersGraph = () => {
             <Tooltip />
             <Legend />
             <Line type="monotone" dataKey="Users" stroke="#8884d8" activeDot={{ r: 8 }} />
-            <Line type="monotone" dataKey="COVID-19 Cases" stroke="#cf1322" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="At Risk Cases" stroke="#cf1322" activeDot={{ r: 8 }} />
          </LineChart>
       </TileContainer>
    );
