11a1fa4f02133063b9d3c4924c07bd1f9cbeae15
Keith Tang <keithtang28@gmail.com>
2020-11-29 19:46:11 -0500

    created stlying file

--- src/components/Dashboard/DateTile.tsx
+++ src/components/Dashboard/DateTile.tsx
@@ -1,27 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const TileContainer = styled.div`
-  height: 130px;
-  width: 270px;
-  background-color: #296157;
-  box-shadow: 2px 2px gainsboro;
-  padding: 15px;
-  border-radius: 10px;
-  text-align: left;
-`;
-
-const DateText = styled.div`
-  color: white;
-  font-size: 30px;
-  margin: 10px;
-`;
-
-const TimeText = styled.div`
-  color: white;
-  font-size: 30px;
-  margin: 20px 10px;
-`;
+import { TileContainer, DateText, TimeText } from './styled';
 
 class DateTile extends React.Component<{}, any> {
   constructor(props: any) {
---
+++ src/components/Dashboard/styled.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const TileContainer = styled.div`
+  height: 130px;
+  width: 270px;
+  background-color: #296157;
+  box-shadow: 2px 2px gainsboro;
+  padding: 15px;
+  border-radius: 10px;
+  text-align: left;
+`;
+
+export const DateText = styled.div`
+  color: white;
+  font-size: 30px;
+  margin: 10px;
+`;
+
+export const TimeText = styled.div`
+  color: white;
+  font-size: 30px;
+  margin: 20px 10px;
+`;
