2ee02397a11e7e7eebab5441c19ccc879f709e84
guptaharsh25 <me@harshgupta.ca>
2021-01-07 16:20:04 -0500

    header redone

---
+++ src/components/Common/Header/Header.tsx
@@ -0,0 +1,59 @@
+import React, { ReactElement } from 'react';
+
+import { connect } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { RootState } from '../../../store';
+
+import { Banner, HeaderButton, MobileBanner, Styledh1 } from './styled';
+import { DirectionalHint, TooltipHost } from '@fluentui/react';
+
+interface HeaderProps {
+  headerTitle?: string;
+}
+
+const mapStateToProps = (state: RootState) => {
+  return {
+    headerTitle: state.page.currentPageTitle,
+  };
+};
+
+const Header: React.FC<HeaderProps> = (props: HeaderProps): ReactElement => {
+  const history = useHistory();
+
+  return (
+    <Banner>
+      <MobileBanner>
+        <Styledh1>
+          {props.headerTitle ? props.headerTitle : 'QuickPass Web Portal'}
+        </Styledh1>
+      </MobileBanner>
+      <MobileBanner>
+        <TooltipHost
+          content="Help"
+          directionalHint={DirectionalHint.bottomCenter}
+        >
+          <HeaderButton
+            iconProps={{ iconName: 'Help' }}
+            title="Help"
+            ariaLabel="Help"
+          />
+        </TooltipHost>
+        <TooltipHost
+          content="Sign Out"
+          directionalHint={DirectionalHint.bottomCenter}
+        >
+          <HeaderButton
+            iconProps={{ iconName: 'SignOut' }}
+            title="Sign Out"
+            ariaLabel="Sign Out"
+            onClick={() => {
+              history.push('/');
+            }}
+          />
+        </TooltipHost>
+      </MobileBanner>
+    </Banner>
+  );
+};
+
+export default connect(mapStateToProps)(Header);
---
+++ src/store/page/actions.ts
@@ -0,0 +1,13 @@
+import { NAVIGATION, PageAction } from './types';
+
+export function setPage(
+  currentPageTitle: string,
+  currentPage: string,
+): PageAction {
+  return {
+    type: NAVIGATION,
+    text: 'The user has changed the page',
+    currentPageTitle: currentPageTitle,
+    currentPage: currentPage,
+  };
+}
