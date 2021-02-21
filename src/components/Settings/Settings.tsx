d452a9be49d8fd80707ae5a088fa66a9016cd1ab
Daniel Shwan <dannyshwan@gmail.com>
2021-02-21 12:32:14 -0500

    added update capacity functionality

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,9 +1,50 @@
 import React from 'react';
-
-import { Display } from './styled';
+import { Card, CardContent, TextField, Button } from '@material-ui/core';
+import { SettingSection, Display } from './styled';
+import { Formik, Form } from 'formik';
+import axios from 'axios';
 
 const Settings = () => {
-  return <Display>Settings</Display>;
+  return (
+    <SettingSection>
+      <Display>
+        <h1>Settings</h1>
+        <Card>
+          <CardContent>
+            <h2>Facility Information</h2>
+            <Formik
+              initialValues={{ capacity: 22 }}
+              onSubmit={async (data) => {
+                await axios.patch(
+                  `${process.env.REACT_APP_API_BASE_URL}/facility/by/602ea8d423a00b4812b77ee6`,
+                  data,
+                );
+              }}
+            >
+              {(props) => (
+                <Form onSubmit={props.handleSubmit}>
+                  <TextField
+                    InputProps={{
+                      inputProps: { min: 1 },
+                    }}
+                    onChange={props.handleChange}
+                    label="Capacity"
+                    type="number"
+                    variant="outlined"
+                    value={props.values.capacity}
+                    name="capacity"
+                  />
+                  <Button type="submit" variant="contained" color="primary">
+                    Update facility
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </CardContent>
+        </Card>
+      </Display>
+    </SettingSection>
+  );
 };
 
 export default Settings;
